de6667708b682470f7eb111d520a25d5714b10c1
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-05-30 12:26:32 -0300

    Testando variavel de ambiente.

--- server-start.js
+++ server-start.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 process.env.SERVER_PORT = process.env.SERVER_PORT || "4040";
-process.env.JWT_SECRET = "0a6b944d-d2fb-46fc-a85e-0295c986cd9f";
+process.env.JWT_SECRET =
+  process.env.JWT_SECRET || "0a6b944d-d2fb-46fc-a85e-0295c986cd9f";
 console.log(`Hi, Deivid. Your JWT_SECRECT is: ${process.env.JWT_SECRET}`);
 process.env.PUBLIC_KEY = `
 -----BEGIN PUBLIC KEY-----
