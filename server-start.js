3ff081ace6ccb5a22d33aee856120e87816365fa
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2020-05-30 23:18:58 -0300

    Variavel de conexão com o banco.

--- server-start.js
+++ server-start.js
@@ -1,8 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 process.env.SERVER_PORT = process.env.SERVER_PORT || "4040";
-process.env.JWT_SECRET =
-  process.env.JWT_SECRET || "0a6b944d-d2fb-46fc-a85e-0295c986cd9f";
-console.log(`Hi, Deivid. Your JWT_SECRECT is: ${process.env.JWT_SECRET}`);
+process.env.JWT_SECRET = "0a6b944d-d2fb-46fc-a85e-0295c986cd9f";
+
 process.env.PUBLIC_KEY = `
 -----BEGIN PUBLIC KEY-----
 MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEh/YHlmq0l2jniPJkix84v6N5PxBs
@@ -11,7 +10,8 @@ MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEh/YHlmq0l2jniPJkix84v6N5PxBs
 `;
 
 process.env.MONGO_HOST =
-  "mongodb://deividr:lu1312enzo@decorp-shard-00-00-qjo1q.mongodb.net:27017,decorp-shard-00-01-qjo1q.mongodb.net:27017,decorp-shard-00-02-qjo1q.mongodb.net:27017/decorp?ssl=true&replicaSet=decorp-shard-0&authSource=admin&retryWrites=false";
+  process.env.MONGO_HOST ||
+  "mongodb://localhost:27017/decorp?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 process.env.MONGO_PORT = "27017";
 require("babel-core/register");
 require("babel-polyfill");
